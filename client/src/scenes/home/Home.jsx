53c5b213d28981a3c13249ef19480f603214a73d
Kapil Jangid <kapiljangid040@gmail.com>
2023-04-24 09:56:24 +0530

    Scenes commited

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,9 +1,13 @@
 import MainCarousel from "./MainCarousel";
+import Subscribe from "./Subscribe";
+import ShoppingList from "./ShoppingList";
 
 const Home = () => {
   return (
     <div classname="home">
       <MainCarousel />
+      <ShoppingList />
+      <Subscribe />
     </div>
   );
 };
